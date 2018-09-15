36787af21e28214095abba10c7f1d71ba90bff22
Erwin Milan <errmilan@gmail.com>
2018-09-15 22:43:29 +0800

    Add files via upload
    Sprint 1 for Database
    This can add or remove the student's name and student number to the database.
    This can also search a student based on his/her last name.

---
+++ student.py
@@ -0,0 +1,17 @@
+class Student:
+
+    def __init__(self, first, last, sn):
+        self.first = first
+        self.last = last
+        self.sn = sn
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.sn)
